b57e8e07b70948ca8e527512ace64d06c8f079ce
akagiyuu <khanghuynh0245@gmail.com>
2025-01-18 09:53:41 +0700

    feat(backend): add CORS

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -10,8 +10,22 @@ const fn default_broadcast_channel_count() -> usize {
     512
 }
 
+fn default_public_cors_domain() -> String {
+    "http://localhost:5173".to_string()
+}
+
+fn default_local_cors_domain() -> String {
+    "http://localhost:5173".to_string()
+}
+
 #[derive(Deserialize)]
 pub struct Config {
+    #[serde(default = "default_public_cors_domain")]
+    pub public_cors_domain: String,
+
+    #[serde(default = "default_local_cors_domain")]
+    pub local_cors_domain: String,
+
     #[serde(default = "default_port")]
     pub port: u16,
 
--- backend/src/server/mod.rs
+++ backend/src/server/mod.rs
@@ -1,20 +1,43 @@
 use std::sync::Arc;
 
 use axum::{
+    http::{
+        header::{
+            ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS,
+            ACCESS_CONTROL_ALLOW_ORIGIN, AUTHORIZATION, CONTENT_TYPE, ORIGIN,
+        },
+        HeaderName, HeaderValue, Method,
+    },
     routing::{get, post},
     Router,
 };
 use doc::ApiDoc;
-use tower_http::trace::TraceLayer;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
-use crate::AppState;
+use crate::{AppState, CONFIG};
 
 mod doc;
 mod handler;
 
+const ALLOW_HEADERS: [HeaderName; 7] = [
+    ORIGIN,
+    AUTHORIZATION,
+    ACCESS_CONTROL_ALLOW_ORIGIN,
+    CONTENT_TYPE,
+    ACCEPT,
+    ACCESS_CONTROL_ALLOW_METHODS,
+    ACCESS_CONTROL_ALLOW_HEADERS,
+];
+const ALLOW_METHODS: [Method; 2] = [Method::GET, Method::POST];
+
 pub fn build(state: Arc<AppState>) -> Router {
+    let allow_origins = [
+        CONFIG.public_cors_domain.parse::<HeaderValue>().unwrap(),
+        CONFIG.local_cors_domain.parse::<HeaderValue>().unwrap(),
+    ];
+
     // register routes
     let router = Router::new()
         .route("/", get(handler::ping))
@@ -22,6 +45,15 @@ pub fn build(state: Arc<AppState>) -> Router {
         .route("/data/{device_name}", get(handler::get_data::get_data))
         .route("/data", post(handler::post_data::post_data))
         .route("/register", post(handler::register::register));
+
+    let router = router.layer(
+        CorsLayer::new()
+            .allow_origin(allow_origins)
+            .allow_headers(ALLOW_HEADERS)
+            .expose_headers(ALLOW_HEADERS)
+            .allow_credentials(true)
+            .allow_methods(ALLOW_METHODS),
+    );
 
     // add openapi doc and swagger
     let router =

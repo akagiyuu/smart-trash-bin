4f7e62e8e54a12b2ad12b585760efc92d8fb895c
akagiyuu <khanghuynh0245@gmail.com>
2025-01-25 00:51:47 +0700

    feat(frontend): use map as device chooser

--- frontend/src/pages/device.tsx
+++ frontend/src/pages/device.tsx
@@ -1,26 +1,51 @@
-import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardDescription,
-    CardContent,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
 import { useToast } from '@/hooks/use-toast';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { SearchBar } from '@/components/search_bar';
+import { latLng, LatLngExpression } from 'leaflet';
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+type Device = {
+    id: string;
+    name: string;
+    latitude: number;
+    longitude: number;
+};
+
+const intoLatLng = (data: { latitude: number; longitude: number }) =>
+    latLng({
+        lat: data.latitude,
+        lng: data.longitude,
+    });
+
+// Default to Ho Chi Minh City
+const DEFAULT_LOCATION = latLng({
+    lat: 10.747538648019852,
+    lng: 106.6762389246497,
+});
+
+function MapController({ location }: { location: LatLngExpression }) {
+    const map = useMap();
+
+    useEffect(() => {
+        map.setView(location);
+    }, [location, map]);
+
+    return null;
+}
 
 export const Device = () => {
+    const [location, setLocation] =
+        useState<LatLngExpression>(DEFAULT_LOCATION);
+    const [error, setError] = useState<GeolocationPositionError | null>(null);
+    const navigate = useNavigate();
+
     const { toast } = useToast();
 
-    const { error, data } = useQuery<string[]>({
+    const { error: fetchError, data } = useQuery<Device[]>({
         queryKey: ['device'],
         queryFn: async () => {
             const response = await fetch(
@@ -30,63 +55,90 @@ export const Device = () => {
             return await response.json();
         },
     });
-    const [selected_device, set_selected_device] = useState<string | null>(
-        null,
-    );
 
-    if (error) {
-        toast({
-            title: 'Failed to get device list',
-            description: error.toString(),
-            variant: 'destructive',
-        });
-        return;
-    }
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => intoLatLng(position.coords),
+            (error) => setError(error),
+        );
+    }, []);
+
+    useEffect(() => {
+        if (error) {
+            toast({
+                title: 'Fallback to default location',
+                description: error.toString(),
+                variant: 'destructive',
+            });
+        }
+    }, [error, toast]);
+
+    useEffect(() => {
+        if (fetchError) {
+            toast({
+                title: 'Failed to get device list',
+                description: fetchError.toString(),
+                variant: 'destructive',
+            });
+        }
+    }, [fetchError, toast]);
 
     if (data === undefined) return;
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <Card className="w-full max-w-sm">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-bold text-center">
-                        Trash Bin Monitoring
-                    </CardTitle>
-                    <CardDescription className="text-center">
-                        Monitor a chosen trash bin
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex items-center space-x-4 mb-6">
-                        <Select
-                            value={selected_device || ''}
-                            onValueChange={(device) =>
-                                set_selected_device(device)
-                            }
+        <div>
+            <SearchBar
+                className="z-[999] fixed top-5 left-1/2 transform -translate-x-1/2"
+                data={data}
+                onSelect={(device) => {
+                    setLocation(intoLatLng(device));
+                }}
+            />
+            <MapContainer
+                center={location}
+                zoom={13}
+                style={{ height: '100vh' }}
+            >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                {data.map((item) => {
+                    return (
+                        <Marker
+                            key={item.id}
+                            position={intoLatLng(item)}
+                            title={item.name}
                         >
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Select a device" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {data.map((item) => (
-                                    <SelectItem key={item} value={item}>
-                                        {item}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <Button
-                        className="w-full"
-                        disabled={!selected_device}
-                        onClick={() =>
-                            window.location.replace(`/${selected_device}`)
-                        }
-                    >
-                        Monitor
-                    </Button>
-                </CardContent>
-            </Card>
+                            <Popup>
+                                <Card className="w-full max-w-sm border-none">
+                                    <CardHeader>
+                                        <CardTitle className="text-center text-lg font-bold">
+                                            {item.name}
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="space-y-2">
+                                        <p className="text-sm">
+                                            Latitude: {item.latitude}
+                                        </p>
+                                        <p className="text-sm">
+                                            Longitude: {item.longitude}
+                                        </p>
+                                        <Button
+                                            variant="default"
+                                            className="w-full"
+                                            onClick={() =>
+                                                navigate(`/${item.id}`)
+                                            }
+                                        >
+                                            Monitor
+                                        </Button>
+                                    </CardContent>
+                                </Card>
+                            </Popup>
+                        </Marker>
+                    );
+                })}
+                <MapController location={location} />
+            </MapContainer>
         </div>
     );
 };
